527f49bfab3228bca223665a69bce6a0fc92d641
Steven Stanfield <stanfield@scarecrowtech.com>
2016-09-04 22:17:01 -0400

    Enable STB fonts.

--- src/tb/tb_config.h
+++ src/tb/tb_config.h
@@ -42,7 +42,7 @@
 	app, that you know work! Freetype generates much prettier glyphs (using
 	hinting) but is a lot larger. This implementation is kept here as alternative
 	as long as it compiles. */
-//#define TB_FONT_RENDERER_STB
+#define TB_FONT_RENDERER_STB
 
 /** Enable to support image loading using stb_image.c (http://nothings.org/).
 	It's a *very unsafe* image library. Use only with images distributed with
